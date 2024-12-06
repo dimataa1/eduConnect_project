1abf50ea147fa979054cbefe9202d299c793b54a
Dimitar Dimov <dimitar200720@gmail.com>
2024-12-06 22:48:56 +0200

    fixed some bugs

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,33 @@
 from django.contrib import admin
-from .models import School
+from .models import School, Post
 
-admin.site.register(School)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subject', 'grade', 'author', 'created_at')
+    list_filter = ('subject', 'grade', 'author', 'created_at')
+    search_fields = ('title', 'author__username', 'subject', 'grade')
+    ordering = ('-created_at',)
+    actions = ['activate_posts', 'deactivate_posts']
+
+    def activate_posts(self, request, queryset):
+        count = queryset.filter(is_active=False).update(is_active=True)
+        self.message_user(request, f"{count} posts activated.")
+
+    activate_posts.short_description = "Activate selected posts"
+
+    def deactivate_posts(self, request, queryset):
+        count = queryset.filter(is_active=True).update(is_active=False)
+        self.message_user(request, f"{count} posts deactivated.")
+
+    deactivate_posts.short_description = "Deactivate selected posts"
+
+
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('name', 'town', 'description', 'image')
+    list_filter = ('town',)
+    search_fields = ('name', 'town', 'description')
+    ordering = ('name',)
