aa961ade19660962937cb485cd4ba8064ff5d38a
Dimitar Dimov <dimitar200720@gmail.com>
2025-01-07 01:15:03 +0200

    made the quiz search and create functionality

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from quiz.views import QuizView, CreateQuizView
+from quiz.views import QuizView, CreateQuizView, QuizSearchAPIView
 
 urlpatterns = [
     path('quiz_home/', QuizView.as_view(), name='quiz_home'),
     path('quiz/create/', CreateQuizView.as_view(), name='create_quiz'),
+    path("api/quiz/search/", QuizSearchAPIView.as_view(), name="search_quizzes"),
+
 ]
