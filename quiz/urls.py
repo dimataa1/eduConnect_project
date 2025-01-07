d24e7a719e68bb0b2dd0689b18424bec9e74a2f6
Dimitar Dimov <dimitar200720@gmail.com>
2025-01-07 23:06:40 +0200

    tried fixing some functionalities, still have to work on them

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from quiz.views import QuizView, CreateQuizView, QuizSearchAPIView
+from quiz.views import (
+    QuizView,
+    CreateQuizView,
+    QuizSearchAPIView,
+    QuizDetailView,
+)
 
 urlpatterns = [
-    path('quiz_home/', QuizView.as_view(), name='quiz_home'),
     path('quiz/create/', CreateQuizView.as_view(), name='create_quiz'),
-    path("api/quiz/search/", QuizSearchAPIView.as_view(), name="search_quizzes"),
-
+    path('api/quiz/search/', QuizSearchAPIView.as_view(), name='search_quizzes'),
+    path('quiz/<int:pk>/', QuizDetailView.as_view(), name='quiz_detail'),
+    path('quiz_home/', QuizView.as_view(), name='quiz_home'),
 ]
