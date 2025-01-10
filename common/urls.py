afa92a65f549ad2f6e8cedad6001257691580951
Dimitar Dimov <dimitar200720@gmail.com>
2025-01-10 12:15:02 +0200

    fixed the eye, still have to redesign , made a calendar,have to fix the nav

--- common/urls.py
+++ common/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from posts.views import DashBoardListView
-from .views import HomeView, PostSearchAPIView, SchoolSearchAPIView
+from .views import HomeView, PostSearchAPIView, SchoolSearchAPIView, AcademicCalendarView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('dashboard/', DashBoardListView.as_view(), name='dashboard'),
     path('api/search/', PostSearchAPIView.as_view(), name='post_search_api'),
     path('api/schools/search/', SchoolSearchAPIView.as_view(), name='school_search_api'),
+    path('calendar/', AcademicCalendarView.as_view(), name='academic_calendar'),
 
 ]
--- common/views.py
+++ common/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import TemplateView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -50,3 +51,7 @@ class SchoolSearchAPIView(APIView):
 
         serializer = SchoolSerializer(schools, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class AcademicCalendarView(TemplateView):
+    template_name = 'common/school_calendar.html'
